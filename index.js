14ddebff091045521048fe779f1c65851a2748a6
Olubukola Ayodele <116734274+ayodelebukola@users.noreply.github.com>
2023-02-17 12:41:18 -0800

    final

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function add(value1, value2) { return value1 + value2 }
-add (10, 20)
+function add(value1, value2) 
+{ return value1 + value2 }
+console.log (add (10, 10))
+
+
+let myname = 'Bukola Ayodele'
+let track = 'Frontend development'
+let location = 'Lagos'
+
+console.log('My name is ' + myname + '. My track is ' + track + '. I live in ' + location + '.')
